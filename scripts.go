e9ced119d9802e62faca042567c17e05d47c870a
Yi Wang <yi.wang.2005@gmail.com>
2014-11-23 20:19:20 -0800

    More progress

---
+++ scripts.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"unicode"
+	"fmt"
+)
+
+func main() {
+	s := "ألسّلام عليكمনমস্কার 你好世界 Hello World"
+	for _, c := range s {
+		for n, s := range unicode.Scripts {
+			if unicode.In(c, s) {
+				fmt.Printf("%c is %s\n", c, n)
+			}
+		}
+	}
+}
